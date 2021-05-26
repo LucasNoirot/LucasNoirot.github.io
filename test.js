96e35111ede324deead31e956bc4570cd03a46ad
LucasNoirot <84563345+LucasNoirot@users.noreply.github.com>
2021-05-26 12:41:57 +0200

    Add files via upload

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
     tableau.extensions.initializeAsync().then(function () {      
         dashboard = tableau.extensions.dashboardContent.dashboard;
 
-        console.log('test 11')
+        console.log('test 12')
         
         //Assigne la vue contenant les données à une variable 
         dashboard.worksheets.forEach(function(worksheet){
@@ -125,7 +125,7 @@ function loadResult(result){
     console.log('Loading result')
     $('#result').DataTable({
         "columns" : cols,
-        "data" : result._value
+        "data" : result._data
     }
     );
     console.log('loaded')
